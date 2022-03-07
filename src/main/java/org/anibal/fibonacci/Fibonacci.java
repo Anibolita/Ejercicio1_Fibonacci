a9483cc8a956628af6b9e31ec4aea0ad1fd95d7d
Anibolita <anibalcapitanreina@uma.es>
2022-03-07 18:39:12 +0100

    Función Fibonacci Realizada

--- src/main/java/org/anibal/fibonacci/Fibonacci.java
+++ src/main/java/org/anibal/fibonacci/Fibonacci.java
@@ -1,4 +1,27 @@
 package org.anibal.fibonacci;
 
 public class Fibonacci {
+
+    public int compute(int number){
+
+        int result;
+
+        if (number>1){
+            result = compute(number-1) + compute(number-2);  //función recursiva
+        }
+        else if (number==1) {  // caso base
+            result = 1;
+        }
+        else if (number==0){  // caso base
+            result = 0;
+        }
+        else{ //error
+            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            result = -1;
+        }
+
+        return result;
+
+    }
+
 }
